3b96f30305a1f2c75e12f1ec3880f65e3a25d67a
charita <charita.sura@gmail.com>
2022-01-23 20:32:27 +0700

    update date

--- README.md
+++
@@ -1,1 +0,0 @@
-Waste-Sorting-Machine-by-Machine-Learning-System
--- WebCEproject/src/App.js
+++ WebCEproject/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Header from './component/Header';
 import Table from './component/Table';
 import Select from './component/Select';
-
+import Date from './component/Date';
+import Graph from './component/Graph';
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
       <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet"></link>
       <Header />
       <Table />
-      
+      <Date />
+      {/* <Graph /> */}
       {/* <Select /> */}
     </div>
     
