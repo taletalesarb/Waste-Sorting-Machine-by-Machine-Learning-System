90b1ab4e66df3fc358b72605116ffb272ddafc88
AomNatthamon <60309850+AomNatthamon@users.noreply.github.com>
2022-03-20 16:00:54 +0700

    update api

--- API/server.py
+++ API/server.py
@@ -1,10 +1,13 @@
 from tabnanny import check
 from flask import Flask, request, Response
-# import jsonpickle
+import jsonpickle
 import numpy as np
 import cv2
 from datetime import datetime
 import os
+from flask_cors import CORS,cross_origin
+
+
 
 now = datetime.today()
 dateNow = now.strftime("%Y-%m-%d")
@@ -23,45 +26,85 @@ if not isExist:
 
 # Initialize the Flask application
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+
+def Home():
+    return "This is Project's API"
 
 # route http posts to this method
+Pictures = []
+@app.route('/api/picture', methods=['POST', 'GET'])
+def Picture():
+    if request.method == 'POST':
+        print("!!!request is coming!!!")
+        r = request
+        x = dict(request.form.items())
+        typeWaste = x.get("type")
+        print(x)
+        # convert string of image data to uint8
+        nparr = np.fromstring(r.files.get("upload_file").read(), np.uint8)
+        # decode image
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        
+        # do some fancy processing here....
+        
+        numg = len(os.listdir(DateFolder+"/general"))
+        numr = len(os.listdir(DateFolder+"/recycle"))
+        numh = len(os.listdir(DateFolder+"/hazardous"))
+        
+        if typeWaste=="general":
+            name = numg + 1
+        elif typeWaste=="recycle":
+            name = numr + 1
+        elif typeWaste=="hazardous":
+            name = numh + 1
+        
+        cv2.imwrite("Picture/"+dateNow+"/"+typeWaste+"/"+str(name)+".jpg",img)
 
-@app.route('/api/test', methods=['POST'])
-def test():
-    print("!!!request is coming!!!")
-    r = request
-    x = dict(request.form.items())
-    typeWaste = x.get("type")
-    print(x)
-    # convert string of image data to uint8
-    nparr = np.fromstring(r.files.get("upload_file").read(), np.uint8)
-    # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    
-    # do some fancy processing here....
-    
-    numg = len(os.listdir(DateFolder+"/general"))
-    numr = len(os.listdir(DateFolder+"/recycle"))
-    numh = len(os.listdir(DateFolder+"/hazardous"))
-    
-    if typeWaste=="general":
-        name = numg + 1
-    elif typeWaste=="recycle":
-        name = numr + 1
-    elif typeWaste=="hazardous":
-        name = numh + 1
-    
-    cv2.imwrite("Picture/"+dateNow+"/"+typeWaste+"/"+str(name)+".jpg",img)
+        # cv2.imwrite("Picture/mm.jpg", img)
 
-    # cv2.imwrite("Picture/mm.jpg", img)
+        # build a response dict to send back to client
+        response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                    }
+        Pictures.append(dateNow+"/"+typeWaste+"/"+str(name)+".jpg")
+        # encode response using jsonpickle
+        response_pickled = jsonpickle.encode(response)
 
-    # build a response dict to send back to client
-    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
-                }
-    # encode response using jsonpickle
-    # response_pickled = jsonpickle.encode(response)
+        return Response(response=response_pickled, status=200, mimetype="application/json")
+    elif request.method == 'GET':
+        return {"Pictures" : Pictures}, 200
 
-    return Response(response=response, status=200, mimetype="application/json")
+
+from flask import send_file, send_from_directory, safe_join, abort
+
+@app.route('/Showpic', methods=['GET'])
+def Showpic():
+    filename = 'Picture/2022-03-07/general/1.jpg'
+    return send_file(filename, mimetype='image/jpg')
+
+
+
+app.config["CLIENT_IMAGES"] = "d:/API/Picture"
+print(app.config["CLIENT_IMAGES"])
+print(os.listdir("d:/API/Picture/2022-03-07/general"))
+
+@app.route("/get-image/<path:image_name>")
+def get_image(image_name):
+    try:  
+        return send_from_directory(app.config["CLIENT_IMAGES"], image_name, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+@app.route("/getnumber")
+@cross_origin()
+def getnumber():
+    numg = len(os.listdir("Picture/2022-03-07/general"))
+    numr = len(os.listdir("Picture/2022-03-07/recycle"))
+    numh = len(os.listdir("Picture/2022-03-07/hazardous"))
+    return {"numg":numg,"numr":numr,"numh":numh}
+        
 
 # start flask app
 app.run(host="0.0.0.0", port=5000)
@@ -70,7 +113,6 @@ app.run(host="0.0.0.0", port=5000)
 # from flask import Flask
 # from flask import render_template
 # from flask import request
-
 
 # app = Flask(__name__)
 
