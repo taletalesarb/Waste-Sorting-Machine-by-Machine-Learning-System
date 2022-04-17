1ebcd363d19e81333e81f4c225b6dcf699915919
AomNatthamon <61010351@kmitl.ac.th>
2022-04-17 22:34:30 +0700

    up date

--- WebCEproject/src/component/Date.js
+++ WebCEproject/src/component/Date.js
@@ -1,14 +1,15 @@
 
-// import TextField from '@mui/material/TextField';
 import './Date.css'
 import * as React from 'react';
 
 import TextField from '@mui/material/TextField';
+
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 
-import { useState } from "react";
+import { styled } from '@mui/material/styles';
+
 
 export default function MaterialUIPickers(props) {
   const { newDate , value} = props;
@@ -16,23 +17,41 @@ export default function MaterialUIPickers(props) {
   var today = new Date();
   var min = (new Date()).setDate(today.getDate() - 7);
 
+
+  const CssTextField = styled(TextField)({
+ 
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        border: '3px solid #b398fc'
+        ,
+      },
+      '&:hover fieldset': {
+        border: '3px solid #01b29a',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'green',
+      },
+      
+    },
+  });
+
   return (
+    <div>
     <div id = "selectDate" >
     <LocalizationProvider dateAdapter={AdapterDateFns} >
-      {/* <Stack spacing={3} > */}
         <DesktopDatePicker
-          
+        
           label="Date"
           inputFormat="dd/MM/yyyy"
           value={value}
           onChange={newDate}
-          
           maxDate= {today}
           minDate = {min}
-          renderInput={(params) => <TextField {...params} id="date" />}
+          renderInput={(params) => <CssTextField {...params} id="date"/>}
         />
     </LocalizationProvider>
     
+    </div>
     </div>
   );
 }
